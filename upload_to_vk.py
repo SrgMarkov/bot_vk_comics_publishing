00d8fa8bff9386aea5d1e58247a1b3882e249282
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-08-04 09:23:37 +0400

    Move picture_file closing after upload_response

--- upload_to_vk.py
+++ upload_to_vk.py
@@ -5,9 +5,8 @@ import requests
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
-
 VK_API_URL = 'https://api.vk.com/method/'
-XKCD_COMICS_COUNT = 1
+XKCD_COMICS_COUNT = 2811
 
 
 def get_file_extension(url):
@@ -43,22 +42,22 @@ if __name__ == '__main__':
     wall_upload_server_response.raise_for_status()
     upload_url = wall_upload_server_response.json()['response']['upload_url']
 
-    with open(picture["picture_file"], 'rb') as picture_file:
+    with open(picture['picture_file'], 'rb') as picture_file:
         upload_files = {'photo': picture_file}
         upload_response = requests.post(upload_url, files=upload_files)
-        upload_response.raise_for_status()
-
-        upload_parameters = {'photo': upload_response.json()['photo'],
-                             'server': upload_response.json()['server'],
-                             'hash': upload_response.json()['hash']}
-        save_wall_photo_response = requests.post(f'{VK_API_URL}photos.saveWallPhoto', params=(upload_parameters | vk_parameters))
-        save_wall_photo_response.raise_for_status()
-        attachments_parameters = save_wall_photo_response.json()["response"][0]
-        post_parameters = {'owner_id': f'-{group_id}',
-                           'from_group': 1,
-                           'message': picture['picture_text'],
-                           'attachments': f'photo{attachments_parameters["owner_id"]}_{attachments_parameters["id"]}'}
-        post_response = requests.post(f'{VK_API_URL}wall.post', params=(post_parameters | vk_parameters))
-        os.remove(picture["picture_file"])
-        post_response.raise_for_status()
-
+    upload_response.raise_for_status()
+    upload_parameters = upload_response.json()
+    save_wall_photo_parameters = {'photo': upload_parameters['photo'],
+                                  'server': upload_parameters['server'],
+                                  'hash': upload_parameters['hash']}
+    save_wall_photo_response = requests.post(f'{VK_API_URL}photos.saveWallPhoto',
+                                             params=(save_wall_photo_parameters | vk_parameters))
+    save_wall_photo_response.raise_for_status()
+    attachments_parameters = save_wall_photo_response.json()["response"][0]
+    post_parameters = {'owner_id': f'-{group_id}',
+                       'from_group': 1,
+                       'message': picture['picture_text'],
+                       'attachments': f'photo{attachments_parameters["owner_id"]}_{attachments_parameters["id"]}'}
+    post_response = requests.post(f'{VK_API_URL}wall.post', params=(post_parameters | vk_parameters))
+    os.remove(picture["picture_file"])
+    post_response.raise_for_status()
