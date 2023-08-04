0d5a1c2babed8fedd63d2ecea56c39865604bfaf
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-08-04 12:25:47 +0400

    Add VKResponseError exception

--- upload_to_vk.py
+++ upload_to_vk.py
@@ -9,6 +9,18 @@ VK_API_URL = 'https://api.vk.com/method/'
 XKCD_COMICS_COUNT = 2811
 
 
+class VKResponseError(TypeError):
+    pass
+
+
+def read_vk_response(self):
+    if 'error' in self.json():
+        raise VKResponseError('Response returned with Error')
+    if 'photo' in self.json():
+        if self.json()['photo'] == '[]':
+            raise VKResponseError('No photo to upload')
+
+
 def get_file_extension(url):
     file_url_path, file_extension = os.path.splitext(urlparse(url).path)
     return file_extension
@@ -33,7 +45,11 @@ def get_comic(page):
 def get_server_url_to_upload(params):
     wall_upload_server_response = requests.get(f'{VK_API_URL}photos.getWallUploadServer', params=params)
     wall_upload_server_response.raise_for_status()
-    return wall_upload_server_response.json()['response']['upload_url']
+    try:
+        read_vk_response(wall_upload_server_response)
+        return wall_upload_server_response.json()['response']['upload_url']
+    except VKResponseError:
+        return exit('get_server_url_to_upload returned with Error')
 
 
 def upload_comic_to_server(params):
@@ -41,27 +57,39 @@ def upload_comic_to_server(params):
         upload_files = {'photo': picture_file}
         upload_response = requests.post(get_server_url_to_upload(params), files=upload_files)
     upload_response.raise_for_status()
-    upload_parameters = upload_response.json()
-    return {'photo': upload_parameters['photo'],
-            'server': upload_parameters['server'],
-            'hash': upload_parameters['hash']}
+    try:
+        read_vk_response(upload_response)
+        upload_parameters = upload_response.json()
+        return {'photo': upload_parameters['photo'],
+                'server': upload_parameters['server'],
+                'hash': upload_parameters['hash']}
+    except VKResponseError:
+        return exit('upload_comic_to_server returned that nothing to upload')
 
 
 def save_comic(params):
     save_wall_photo_response = requests.post(f'{VK_API_URL}photos.saveWallPhoto',
                                              params=(upload_comic_to_server(params) | params))
     save_wall_photo_response.raise_for_status()
-    attachments_parameters = save_wall_photo_response.json()["response"][0]
-    return {'owner_id': f'-{group_id}',
-            'from_group': 1,
-            'message': picture['picture_text'],
-            'attachments': f'photo{attachments_parameters["owner_id"]}_{attachments_parameters["id"]}'}
+    try:
+        read_vk_response(save_wall_photo_response)
+        attachments_parameters = save_wall_photo_response.json()["response"][0]
+        return {'owner_id': f'-{group_id}',
+                'from_group': 1,
+                'message': picture['picture_text'],
+                'attachments': f'photo{attachments_parameters["owner_id"]}_{attachments_parameters["id"]}'}
+    except VKResponseError:
+        return exit('save_comic returned with Error')
 
 
 def post_comic_in_vk_wall(params):
     post_response = requests.post(f'{VK_API_URL}wall.post', params=(save_comic(params) | params))
     os.remove(picture["picture_file"])
     post_response.raise_for_status()
+    try:
+        read_vk_response(post_response)
+    except VKResponseError:
+        return exit('post_comic_in_vk_wall returned with Error')
 
 
 if __name__ == '__main__':
@@ -71,4 +99,3 @@ if __name__ == '__main__':
     picture = get_comic(randint(1, XKCD_COMICS_COUNT))
     vk_parameters = {'access_token': vk_token, 'v': 5.131, 'group_id': group_id}
     post_comic_in_vk_wall(vk_parameters)
-
